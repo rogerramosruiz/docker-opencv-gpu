b17301b37d36a71dd718a98eca62b92152ef8af0
rogerramosruiz <rogerramosruizdev@gmail.com>
2023-04-10 21:56:47 -0400

    cuda version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import subprocess
+import re 
 
 def nvidia_smi(query = None):
     command = 'nvidia-smi'
@@ -7,6 +8,15 @@ def nvidia_smi(query = None):
         command = command.split()
     return subprocess.check_output(command)
 
+def cuda_version():
+    data = nvidia_smi()
+    data = data.decode('utf-8').strip()
+    version = re.findall("CUDA Version: \d{1,2}\.\d{1,2}", data)[0]
+    version = re.findall("\d{1,2}.\d{1,2}", version)[0]
+    if len(version.split('.')) < 3:
+        version += '.0'
+    return version
+
 
 if __name__ == '__main__':
-    print(nvidia_smi())
+    print(cuda_version())
